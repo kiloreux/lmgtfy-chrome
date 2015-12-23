7444429e0ba72194f11a6d3ddbd7f9a83fe9372b
Kiloreux <kiloreux@gmail.com>
2015-12-23 01:36:14 +0100

    Added copy to clipboard functionality
    Added copy to clipboard functionality, it seems to be working on windows
    only , need be further investigated

--- background.js
+++ background.js
@@ -13,5 +13,18 @@ function lmgtfy(info)
 {
     var data = encodeURIComponent(info.selectionText);
     var link = urlQuery.concat(data);
-
+    copyToClipboard(link);
 }
+
+
+
+function copyToClipboard(text) {
+  const input = document.createElement('input');
+  input.style.position = 'fixed';
+  input.style.opacity = 0;
+  input.value = text;
+  document.body.appendChild(input);
+  input.select();
+  document.execCommand('Copy');
+  document.body.removeChild(input);
+};
